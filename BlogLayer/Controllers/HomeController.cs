6231901e1f44f9d0760ae9cf98c4e02e08059be7
hasankavalci <hsnkvl1996@gmail.com>
2017-07-13 16:31:34 +0300

    asd

---
+++ BlogLayer/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogLayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //asd
+            return View();
+        }
+    }
+}
---
+++ BlogLayer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlogLayer.MvcApplication" Language="C#" %>
